1bc8de3d6eabb64832643415d6b1dbeb460812ea
gargantua <istroganovi@gmail.com>
2019-01-12 11:46:47 +0300

    initial commit

---
+++ game.rb
@@ -0,0 +1,69 @@
+class Game
+  attr_reader :status, :errors, :letters, :good_letters, :bad_letters
+
+  def initialize(slovo)
+    @letters = get_letters(slovo)
+    @errors = 0
+    @good_letters = []
+    @bad_letters = []
+
+    @status = 0
+  end
+
+  def get_letters(slovo)
+    if slovo == nil || slovo == ""
+      abort "Загадано пустое слово, нечего отгадывать. Закрываемся"
+    end
+
+    slovo.encode('UTF-8').downcase.split("")
+  end
+
+  def next_step(bukva)
+    return if @status == -1 || @status == 1
+
+    # Если введенная буква уже есть в списке "правильных" или "ошибочных" букв,
+    # то ничего не изменилось, выходим из метода.
+    return if @good_letters.include?(bukva) || @bad_letters.include?(bukva)
+
+    if @letters.include?(bukva) ||
+     (bukva == "е" && letters.include?("ё")) ||
+     (bukva == "ё" && letters.include?("е")) ||
+     (bukva == "и" && letters.include?("й")) ||
+     (bukva == "й" && letters.include?("и"))
+
+      @good_letters << bukva
+      add_similar_letters(bukva, good_letters)
+
+      @status = 1 if (@letters.uniq.sort - @good_letters.uniq.sort).empty?
+    else
+      @bad_letters << bukva
+      add_similar_letters(bukva, bad_letters)
+      @errors += 1
+      # Если ошибок больше 7 — статус игры меняем на -1, проигрыш.
+      @status = -1 if @errors >= 7
+    end
+  end
+
+  # Метод, спрашивающий юзера букву и возвращающий ее как результат.
+  def ask_next_letter
+    puts "\nВведите следующую букву"
+
+    letter = ""
+
+    while letter == ""
+      letter = STDIN.gets.encode("UTF-8").chomp.downcase
+    end
+    # После получения ввода, передаем управление в основной метод игры
+    next_step(letter)
+  end
+
+  # Метод проверки и добавления похожих букв в массивы отгаданных и ошибочных
+  def add_similar_letters(letter, array)
+    case letter
+    when "е" then array << "ё"
+    when "ё" then array << "е"
+    when "и" then array << "й"
+    when "й" then array << "и"
+    end
+  end
+end
---
+++ viselitsa.rb
@@ -0,0 +1,31 @@
+# Необходимо для работы программы на ОС Windows
+#
+if Gem.win_platform?
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
+
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
+end
+
+require_relative "game"
+require_relative "result_printer"
+require_relative "word_reader"
+
+puts "Игра виселица. Версия 3.1.1"
+sleep 1
+
+printer = ResultPrinter.new
+word_reader = WordReader.new
+
+words_file_name = File.dirname(__FILE__) + "/data/words.txt"
+
+game = Game.new(word_reader.read_from_file(words_file_name))
+
+while game.status.zero?
+  printer.print_status(game)
+  game.ask_next_letter
+end
+
+printer.print_status(game)
