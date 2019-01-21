69aa2ebb902d9de819d7af3d395d1d11387eb718
gargantua <istroganovi@gmail.com>
2019-01-21 12:04:41 +0300

    refactoring lib files

--- lib/word_reader.rb
+++ lib/word_reader.rb
@@ -4,13 +4,11 @@ class WordReader
   end
 
   def read_from_file(file_name)
-    begin
-      file = File.new(file_name, "r:UTF-8")
-      lines = file.readlines
-      file.close
-    rescue SystemCallError
-      abort "Файл со словами не найден!"
-    end
+    return nil unless File.exist?(file_name)
+
+    file = File.new(file_name, "r:UTF-8")
+    lines = file.readlines
+    file.close
 
     lines.sample.chomp
   end
